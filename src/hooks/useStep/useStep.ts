5804bcc3c8423bda7ba3e98ee13a68c1ad2b0b47
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 13:00:33 -0300

    chore(docs): added jsdocs to useStep

--- src/hooks/useStep/useStep.ts
+++ src/hooks/useStep/useStep.ts
@@ -1,54 +1,85 @@
-import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react'
+import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react';
 
 interface Helpers {
-  goToNextStep: () => void
-  goToPrevStep: () => void
-  reset: () => void
-  canGoToNextStep: boolean
-  canGoToPrevStep: boolean
-  setStep: Dispatch<SetStateAction<number>>
+  goToNextStep: () => void;
+  goToPrevStep: () => void;
+  reset: () => void;
+  canGoToNextStep: boolean;
+  canGoToPrevStep: boolean;
+  setStep: Dispatch<SetStateAction<number>>;
 }
 
+/**
+ * A simple abstraction to play with a stepper, don't repeat yourself.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-step
+ *
+ * @example
+ * export default function Component() {
+ *   const [currentStep, helpers] = useStep(5)
+ *
+ *   const {
+ *     canGoToPrevStep,
+ *     canGoToNextStep,
+ *     goToNextStep,
+ *     goToPrevStep,
+ *     reset,
+ *     setStep,
+ *   } = helpers
+ *
+ *   return (
+ *     <>
+ *       <p>Current step is {currentStep}</p>
+ *       <p>Can go to previous step {canGoToPrevStep ? 'yes' : 'no'}</p>
+ *       <p>Can go to next step {canGoToNextStep ? 'yes' : 'no'}</p>
+ *       <button onClick={goToNextStep}>Go to next step</button>
+ *       <button onClick={goToPrevStep}>Go to previous step</button>
+ *       <button onClick={reset}>Reset</button>
+ *       <button onClick={() => setStep(3)}>Set to step 3</button>
+ *     </>
+ *   )
+ * }
+ */
 function useStep(maxStep: number): [number, Helpers] {
-  const [currentStep, setCurrentStep] = useState(1)
+  const [currentStep, setCurrentStep] = useState(1);
 
   const canGoToNextStep = useMemo(
     () => currentStep + 1 <= maxStep,
     [currentStep, maxStep],
-  )
+  );
 
-  const canGoToPrevStep = useMemo(() => currentStep - 1 >= 1, [currentStep])
+  const canGoToPrevStep = useMemo(() => currentStep - 1 >= 1, [currentStep]);
 
   const setStep = useCallback(
-    step => {
+    (step) => {
       // Allow value to be a function so we have the same API as useState
-      const newStep = step instanceof Function ? step(currentStep) : step
+      const newStep = step instanceof Function ? step(currentStep) : step;
 
       if (newStep >= 1 && newStep <= maxStep) {
-        setCurrentStep(newStep)
-        return
+        setCurrentStep(newStep);
+        return;
       }
 
-      throw new Error('Step not valid')
+      throw new Error('Step not valid');
     },
     [maxStep, currentStep],
-  )
+  );
 
   const goToNextStep = useCallback(() => {
     if (canGoToNextStep) {
-      setCurrentStep(step => step + 1)
+      setCurrentStep((step) => step + 1);
     }
-  }, [canGoToNextStep])
+  }, [canGoToNextStep]);
 
   const goToPrevStep = useCallback(() => {
     if (canGoToPrevStep) {
-      setCurrentStep(step => step - 1)
+      setCurrentStep((step) => step - 1);
     }
-  }, [canGoToPrevStep])
+  }, [canGoToPrevStep]);
 
   const reset = useCallback(() => {
-    setCurrentStep(1)
-  }, [])
+    setCurrentStep(1);
+  }, []);
 
   return [
     currentStep,
@@ -60,7 +91,7 @@ function useStep(maxStep: number): [number, Helpers] {
       setStep,
       reset,
     },
-  ]
+  ];
 }
 
-export default useStep
+export default useStep;
