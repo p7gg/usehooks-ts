7e403c3e349c7afcc5cff03cda3f514dc3fff0e4
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:38:43 -0300

    chore(docs): added jsdocs to useIsomorphicLayoutEffect

--- src/hooks/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.ts
+++ src/hooks/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.ts
@@ -1,6 +1,22 @@
-import { useEffect, useLayoutEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react';
 
+/**
+ * This hook fixes this problem by switching between useEffect and useLayoutEffect following the execution environment.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
+ *
+ * @example
+ * export default function Component() {
+ *   useIsomorphicLayoutEffect(() => {
+ *     console.log(
+ *       "In the browser, I'm an `useLayoutEffect`, but in SSR, I'm an `useEffect`.",
+ *     )
+ *   }, [])
+ *
+ *   return <p>Hello, world</p>
+ * }
+ */
 const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect
+  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
-export default useIsomorphicLayoutEffect
+export default useIsomorphicLayoutEffect;
