762bafd24ee102f97d135e23b091c16649d5e4cd
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:43:43 -0300

    chore(docs): added jsdocs to useLockedBody

--- src/hooks/useLockedBody/useLockedBody.ts
+++ src/hooks/useLockedBody/useLockedBody.ts
@@ -1,53 +1,115 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
-type ReturnType = [boolean, (locked: boolean) => void]
+type ReturnType = [boolean, (locked: boolean) => void];
 
+/**
+ * This React hook is used to block the scrolling of the page.
+ *
+ * A good example of a use case is when you need to open a modal.
+ *
+ * For flexibility, this hook offers 2 APIs:
+ *
+ * - Use it as we would use a useState (example 1)
+ * - Use it with our own logic, coming from a props or redux for example (example 2)
+ *
+ * Finally, you can optionally change the reflow padding if you have another sidebar size than the default (15px)
+ *
+ * @see https://usehooks-ts.com/react-hook/use-locked-body
+ *
+ * @example
+ * const fixedCenterStyle: CSSProperties = {
+ *   position: 'fixed',
+ *   top: '50%',
+ *   left: '50%',
+ *   transform: 'translate(-50%, -50%)',
+ * }
+ *
+ * const fakeScrollableStyle: CSSProperties = {
+ *   minHeight: '150vh',
+ *   background: 'linear-gradient(palegreen, palegoldenrod, palevioletred)',
+ * }
+ *
+ * // Example 1: useLockedBody as useState()
+ * export default function App() {
+ *   const [locked, setLocked] = useLockedBody()
+ *
+ *   const toggleLocked = () => {
+ *     setLocked(!locked)
+ *   }
+ *
+ *   return (
+ *     <div style={fakeScrollableStyle}>
+ *       <button style={fixedCenterStyle} onClick={toggleLocked}>
+ *         {locked ? 'unlock scroll' : 'lock scroll'}
+ *       </button>
+ *     </div>
+ *   )
+ * }
+ *
+ * // Example 2: useLockedBody with our custom state
+ * export function App2() {
+ *   const [locked, setLocked] = useState(false)
+ *
+ *   const toggleLocked = () => {
+ *     setLocked(!locked)
+ *   }
+ *
+ *   useLockedBody(locked)
+ *
+ *   return (
+ *     <div style={fakeScrollableStyle}>
+ *       <button style={fixedCenterStyle} onClick={toggleLocked}>
+ *         {locked ? 'unlock scroll' : 'lock scroll'}
+ *       </button>
+ *     </div>
+ *   )
+ * }
+ */
 function useLockedBody(initialLocked = false): ReturnType {
-  const [locked, setLocked] = useState(initialLocked)
+  const [locked, setLocked] = useState(initialLocked);
 
   // Do the side effect before render
   useIsomorphicLayoutEffect(() => {
     if (!locked) {
-      return
+      return;
     }
 
     // Save initial body style
-    const originalOverflow = document.body.style.overflow
-    const originalPaddingRight = document.body.style.paddingRight
+    const originalOverflow = document.body.style.overflow;
+    const originalPaddingRight = document.body.style.paddingRight;
 
     // Lock body scroll
-    document.body.style.overflow = 'hidden'
+    document.body.style.overflow = 'hidden';
 
     // Get the scrollBar width
-    const root = document.getElementById('___gatsby') // or root
-    const scrollBarWidth = root ? root.offsetWidth - root.scrollWidth : 0
+    const root = document.getElementById('___gatsby'); // or root
+    const scrollBarWidth = root ? root.offsetWidth - root.scrollWidth : 0;
 
     // Avoid width reflow
     if (scrollBarWidth) {
-      document.body.style.paddingRight = `${scrollBarWidth}px`
+      document.body.style.paddingRight = `${scrollBarWidth}px`;
     }
 
     return () => {
-      document.body.style.overflow = originalOverflow
+      document.body.style.overflow = originalOverflow;
 
       if (scrollBarWidth) {
-        document.body.style.paddingRight = originalPaddingRight
+        document.body.style.paddingRight = originalPaddingRight;
       }
-    }
-  }, [locked])
+    };
+  }, [locked]);
 
   // Update state if initialValue changes
   useEffect(() => {
     if (locked !== initialLocked) {
-      setLocked(initialLocked)
+      setLocked(initialLocked);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initialLocked])
+  }, [initialLocked]);
 
-  return [locked, setLocked]
+  return [locked, setLocked];
 }
 
-export default useLockedBody
+export default useLockedBody;
