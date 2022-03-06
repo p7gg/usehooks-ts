62e4c7959ef9b4952c5cf528b1e48b748f83480c
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:31:57 -0300

    chore(docs): added jsdocs to useIsFirstRender

--- src/hooks/useIsFirstRender/useIsFirstRender.ts
+++ src/hooks/useIsFirstRender/useIsFirstRender.ts
@@ -1,15 +1,41 @@
-import { useRef } from 'react'
+import { useRef } from 'react';
 
+/**
+ * Simple React hook that return a boolean;
+ *
+ * - True at the mount time
+ * - Then always false
+ *
+ * @see https://usehooks-ts.com/react-hook/use-is-first-render
+ *
+ * @example
+ * export default function Component() {
+ *   const isFirst = useIsFirstRender()
+ *   const [data, setData] = useState<number>(0)
+ *
+ *   useEffect(() => {
+ *     console.log('Normal useEffect', { data })
+ *   }, [data])
+ *
+ *   return (
+ *     <div>
+ *       <p>Open your console</p>
+ *       <p>Is first render: {isFirst ? 'yes' : 'no'}</p>
+ *       <button onClick={() => setData(Date.now())}>Update data</button>
+ *     </div>
+ *   )
+ * }
+ */
 function useIsFirstRender(): boolean {
-  const isFirst = useRef(true)
+  const isFirst = useRef(true);
 
   if (isFirst.current) {
-    isFirst.current = false
+    isFirst.current = false;
 
-    return true
+    return true;
   }
 
-  return isFirst.current
+  return isFirst.current;
 }
 
-export default useIsFirstRender
+export default useIsFirstRender;
