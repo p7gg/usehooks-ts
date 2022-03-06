659beb1aa6c4c36bcbd54c4719920ae35840f60a
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 11:27:40 -0300

    chore(docs): added jsdocs to useCountdown

--- src/hooks/useCountdown/useCountdown.ts
+++ src/hooks/useCountdown/useCountdown.ts
@@ -1,57 +1,78 @@
 // See: https://usehooks-ts.com/react-hook/use-boolean
-import { useBoolean } from '../useBoolean'
+import { useBoolean } from '../useBoolean';
 // See: https://usehooks-ts.com/react-hook/use-counter
-import { useCounter } from '../useCounter'
+import { useCounter } from '../useCounter';
 // See: https://usehooks-ts.com/react-hook/use-interval
-import { useInterval } from '../useInterval'
+import { useInterval } from '../useInterval';
 
 interface UseCountdownType {
-  seconds: number
-  interval: number
-  isIncrement?: boolean
+  seconds: number;
+  interval: number;
+  isIncrement?: boolean;
 }
 interface CountdownHelpers {
-  start: () => void
-  stop: () => void
-  reset: () => void
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
 }
 
 /**
+ * A simple countdown implementation. Support increment and decrement.
  *
- * @param seconds the countdown's number, generally time seconds
- * @param interval the countdown's interval, milliseconds
- * @param isIncrement determine the countdown is increment, otherwise is decrement
- * @returns
+ * @see https://usehooks-ts.com/react-hook/use-countdown
+ *
+ * @example
+ * export default function Component() {
+ *   const [intervalValue, setIntervalValue] = useState<number>(500)
+ *   const [count, { start, stop, reset }] = useCountdown({
+ *     seconds: 60,
+ *     interval: 500,
+ *     isIncrement: false,
+ *   })
+ *
+ *   const handleChangeIntervalValue = (event: ChangeEvent<HTMLInputElement>) => {
+ *     setIntervalValue(Number(event.target.value))
+ *   }
+ *   return (
+ *     <div>
+ *       <p>Count: {count}</p>
+ *
+ *       <input
+ *         type="number"
+ *         value={intervalValue}
+ *         onChange={handleChangeIntervalValue}
+ *       />
+ *       <button onClick={start}>start</button>
+ *       <button onClick={stop}>stop</button>
+ *       <button onClick={reset}>reset</button>
+ *     </div>
+ *   )
+ * }
  */
 function useCountdown({
   seconds,
   interval,
   isIncrement,
 }: UseCountdownType): [number, CountdownHelpers] {
-  const {
-    count,
-    increment,
-    decrement,
-    reset: resetCounter,
-  } = useCounter(seconds)
+  const { count, increment, decrement, reset: resetCounter } = useCounter(seconds);
   /**
    * Note: used to control the useInterval
    * running: If true, the interval is running
    * start: Should set running true to trigger interval
    * stop: Should set running false to remove interval
    */
-  const { value: running, setTrue: start, setFalse: stop } = useBoolean(false)
+  const { value: running, setTrue: start, setFalse: stop } = useBoolean(false);
 
   /**
    * Will set running false and reset the seconds to initial value
    */
   const reset = () => {
-    stop()
-    resetCounter()
-  }
+    stop();
+    resetCounter();
+  };
 
-  useInterval(isIncrement ? increment : decrement, running ? interval : null)
-  return [count, { start, stop, reset }]
+  useInterval(isIncrement ? increment : decrement, running ? interval : null);
+  return [count, { start, stop, reset }];
 }
 
-export default useCountdown
+export default useCountdown;
--- src/hooks/useCounter/useCounter.ts
+++ src/hooks/useCounter/useCounter.ts
@@ -11,24 +11,6 @@ type IUseCounter = {
 /**
  * Classic counter example to help understand the flow of this npm package
  *
- * @param    {number} initialValue
- *           initial counter value
- *
- * @return   {Object}
- *           object with count and methods
- *
- * @property {number} count
- *           The current count state
- *
- * @property {()=>void} increment
- *           the increment function
- *
- * @property {()=>void} decrement
- *           the decrement function
- *
- * @property {()=>void} reset
- *           the reset function
- *
  * @example
  *   const ExampleComponent = () => {
  *     const { count, increment, reset, decrement } = useCounter();
