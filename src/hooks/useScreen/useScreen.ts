ec438f7fea424f2d3947183232e088959a4ea4a7
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:54:08 -0300

    chore(docs): added jsdocs to useScreen

--- src/hooks/useScreen/useScreen.ts
+++ src/hooks/useScreen/useScreen.ts
@@ -1,33 +1,52 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 // See: https://usehooks-ts.com/react-hook/use-event-listener
-import { useEventListener } from '../useEventListener'
+import { useEventListener } from '../useEventListener';
 // See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
+/**
+ * Easily retrieve `window.screen` object with this Hook React which also works onRezise.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-screen
+ *
+ * @example
+ * export default function Component() {
+ *   const screen = useScreen()
+ *
+ *   return (
+ *     <div>
+ *       The current window dimensions are:{' '}
+ *       <code>
+ *         {JSON.stringify({ width: screen?.width, height: screen?.height })}
+ *       </code>
+ *     </div>
+ *   )
+ * }
+ */
 function useScreen() {
   const getScreen = () => {
     if (typeof window !== 'undefined' && window.screen) {
-      return window.screen
+      return window.screen;
     }
-    return undefined
-  }
+    return undefined;
+  };
 
-  const [screen, setScreen] = useState<Screen | undefined>(getScreen())
+  const [screen, setScreen] = useState<Screen | undefined>(getScreen());
 
   function handleSize() {
-    setScreen(getScreen())
+    setScreen(getScreen());
   }
 
-  useEventListener('resize', handleSize)
+  useEventListener('resize', handleSize);
 
   // Set size at the first client-side load
   useIsomorphicLayoutEffect(() => {
-    handleSize()
+    handleSize();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
-  return screen
+  return screen;
 }
 
-export default useScreen
+export default useScreen;
