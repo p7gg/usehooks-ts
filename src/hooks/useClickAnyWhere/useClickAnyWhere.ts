fe0fe2b7f5992a207c79a4f240a135bd93e30198
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 11:17:50 -0300

    chore(docs): added jsdocs to useClickAnyWhere

--- src/hooks/useClickAnyWhere/useClickAnyWhere.ts
+++ src/hooks/useClickAnyWhere/useClickAnyWhere.ts
@@ -1,11 +1,27 @@
-import { useEventListener } from '../useEventListener'
+import { useEventListener } from '../useEventListener';
 
-type Handler = (event: MouseEvent) => void
+type Handler = (event: MouseEvent) => void;
 
+/**
+ * This simple React hook offers you a click event listener at the page level, don't repeat yourself.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-click-any-where
+ *
+ * @example
+ * export default function Component() {
+ *   const [count, setCount] = useState(0)
+ *
+ *   useClickAnyWhere(() => {
+ *     setCount(prev => prev + 1)
+ *   })
+ *
+ *   return <p>Click count: {count}</p>
+ * }
+ */
 function useClickAnyWhere(handler: Handler) {
-  useEventListener('click', event => {
-    handler(event)
-  })
+  useEventListener('click', (event) => {
+    handler(event);
+  });
 }
 
-export default useClickAnyWhere
+export default useClickAnyWhere;
