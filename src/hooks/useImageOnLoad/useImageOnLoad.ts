260eeef583701b8c22d30bd47c03d8e5d47c933e
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:22:19 -0300

    chore(docs): added jsdocs to useImageOnLoad

--- src/hooks/useImageOnLoad/useImageOnLoad.ts
+++ src/hooks/useImageOnLoad/useImageOnLoad.ts
@@ -1,22 +1,68 @@
-import { CSSProperties, useState } from 'react'
+import { CSSProperties, useState } from 'react';
 
 interface ImageStyle {
-  thumbnail: CSSProperties
-  fullSize: CSSProperties
+  thumbnail: CSSProperties;
+  fullSize: CSSProperties;
 }
 
 interface ImageOnLoadType {
-  handleImageOnLoad: () => void
-  css: ImageStyle
+  handleImageOnLoad: () => void;
+  css: ImageStyle;
 }
 
+/**
+ * A simple React Hook that helps you improve UX while images are loading. Rather than having an image that "unfolds" from top to bottom,
+ * we load a smaller version first which will be blurred and which will be replaced by the normal size image once loaded.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-image-on-load
+ *
+ * @example
+ * export default function Component() {
+ *   const { handleImageOnLoad, css } = useImageOnLoad()
+ *
+ *   const style: { [key: string]: CSSProperties } = {
+ *     wrap: {
+ *       position: 'relative',
+ *       width: 400,
+ *       height: 400,
+ *       margin: 'auto',
+ *     },
+ *     image: {
+ *       position: 'absolute',
+ *       top: 0,
+ *       left: 0,
+ *       width: `100%`,
+ *       height: `100%`,
+ *     },
+ *   }
+ *
+ *   return (
+ *     <div style={style.wrap}>
+ *       // Small image load fast
+ *       <img
+ *         style={{ ...style.image, ...css.thumbnail }}
+ *         src="https://via.placeholder.com/150"
+ *         alt="thumbnail"
+ *       />
+ *
+ *       // Full size image
+ *       <img
+ *         onLoad={handleImageOnLoad}
+ *         style={{ ...style.image, ...css.fullSize }}
+ *         src="https://via.placeholder.com/600"
+ *         alt="fullImage"
+ *       />
+ *     </div>
+ *   )
+ * }
+ */
 function useImageOnLoad(): ImageOnLoadType {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   // Triggered when full image will be loaded.
   const handleImageOnLoad = () => {
-    setIsLoaded(true)
-  }
+    setIsLoaded(true);
+  };
 
   const css: ImageStyle = {
     // Thumbnail style.
@@ -30,9 +76,9 @@ function useImageOnLoad(): ImageOnLoadType {
       opacity: isLoaded ? 1 : 0,
       transition: 'opacity 500ms ease-in 0ms',
     },
-  }
+  };
 
-  return { handleImageOnLoad, css }
+  return { handleImageOnLoad, css };
 }
 
-export default useImageOnLoad
+export default useImageOnLoad;
