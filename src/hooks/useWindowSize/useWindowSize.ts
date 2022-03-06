982d369cc6ca5efe8c94d42084e29a0a342b2fc0
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 13:11:02 -0300

    chore(docs): added jsdocs to useWindowSize

--- src/hooks/useWindowSize/useWindowSize.ts
+++ src/hooks/useWindowSize/useWindowSize.ts
@@ -1,37 +1,52 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-// See: https://usehooks-ts.com/react-hook/use-event-listener
-import { useEventListener } from '../useEventListener'
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+import { useEventListener } from '../useEventListener';
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
 interface WindowSize {
-  width: number
-  height: number
+  width: number;
+  height: number;
 }
 
+/**
+ * Easily retrieve window dimensions with this React Hook which also works onRezise.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-window-size
+ *
+ * @example
+ * export default function Component() {
+ *   const { width, height } = useWindowSize()
+ *
+ *   return (
+ *     <div>
+ *       The current window dimensions are:{' '}
+ *       <code>{JSON.stringify({ width, height })}</code>
+ *     </div>
+ *   )
+ * }
+ */
 function useWindowSize(): WindowSize {
   const [windowSize, setWindowSize] = useState<WindowSize>({
     width: 0,
     height: 0,
-  })
+  });
 
   const handleSize = () => {
     setWindowSize({
       width: window.innerWidth,
       height: window.innerHeight,
-    })
-  }
+    });
+  };
 
-  useEventListener('resize', handleSize)
+  useEventListener('resize', handleSize);
 
   // Set size at the first client-side load
   useIsomorphicLayoutEffect(() => {
-    handleSize()
+    handleSize();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
-  return windowSize
+  return windowSize;
 }
 
-export default useWindowSize
+export default useWindowSize;
