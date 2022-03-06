24971f8597d52a53bf86e1f63a34f7c73a044e59
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 11:53:43 -0300

    chore(docs): added jsdocs to useHover

--- src/hooks/useHover/useHover.ts
+++ src/hooks/useHover/useHover.ts
@@ -1,20 +1,37 @@
-import { RefObject, useState } from 'react'
+import { RefObject, useState } from 'react';
 
-// See: https://usehooks-ts.com/react-hook/use-event-listener
-import { useEventListener } from '../useEventListener'
+import { useEventListener } from '../useEventListener';
 
+/**
+ * React UI sensor hook that determine if the mouse element is in the hover element using Javascript Typescript instead CSS.
+ * This way you can separate the logic from the UI.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-hover
+ *
+ * @example
+ * export default function Component() {
+ *   const hoverRef = useRef(null)
+ *   const isHover = useHover(hoverRef)
+ *
+ *   return (
+ *     <div ref={hoverRef}>
+ *       {`The current div is ${isHover ? `hovered` : `unhovered`}`}
+ *     </div>
+ *   )
+ * }
+ */
 function useHover<T extends HTMLElement = HTMLElement>(
   elementRef: RefObject<T>,
 ): boolean {
-  const [value, setValue] = useState<boolean>(false)
+  const [value, setValue] = useState<boolean>(false);
 
-  const handleMouseEnter = () => setValue(true)
-  const handleMouseLeave = () => setValue(false)
+  const handleMouseEnter = () => setValue(true);
+  const handleMouseLeave = () => setValue(false);
 
-  useEventListener('mouseenter', handleMouseEnter, elementRef)
-  useEventListener('mouseleave', handleMouseLeave, elementRef)
+  useEventListener('mouseenter', handleMouseEnter, elementRef);
+  useEventListener('mouseleave', handleMouseLeave, elementRef);
 
-  return value
+  return value;
 }
 
-export default useHover
+export default useHover;
