306f007c535594889de473e41a7dcee9b30ddef6
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:47:46 -0300

    chore(docs): added jsdocs to useMediaQuery

--- src/hooks/useMediaQuery/useMediaQuery.ts
+++ src/hooks/useMediaQuery/useMediaQuery.ts
@@ -1,36 +1,52 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
+/**
+ * Easily retrieve media dimensions with this Hook React which also works onResize.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-media-query
+ *
+ * @example
+ * export default function Component() {
+ *   const matches = useMediaQuery('(min-width: 768px)')
+ *
+ *   return (
+ *     <div>
+ *       {`The view port is ${matches ? 'at least' : 'less than'} 768 pixels wide`}
+ *     </div>
+ *   )
+ * }
+ */
 function useMediaQuery(query: string): boolean {
   const getMatches = (query: string): boolean => {
     // Prevents SSR issues
     if (typeof window !== 'undefined') {
-      return window.matchMedia(query).matches
+      return window.matchMedia(query).matches;
     }
-    return false
-  }
+    return false;
+  };
 
-  const [matches, setMatches] = useState<boolean>(getMatches(query))
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
 
   function handleChange() {
-    setMatches(getMatches(query))
+    setMatches(getMatches(query));
   }
 
   useEffect(() => {
-    const matchMedia = window.matchMedia(query)
+    const matchMedia = window.matchMedia(query);
 
     // Triggered at the first client-side load and if query changes
-    handleChange()
+    handleChange();
 
     // Listen matchMedia
-    matchMedia.addEventListener('change', handleChange)
+    matchMedia.addEventListener('change', handleChange);
 
     return () => {
-      matchMedia.removeEventListener('change', handleChange)
-    }
+      matchMedia.removeEventListener('change', handleChange);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query])
+  }, [query]);
 
-  return matches
+  return matches;
 }
 
-export default useMediaQuery
+export default useMediaQuery;
