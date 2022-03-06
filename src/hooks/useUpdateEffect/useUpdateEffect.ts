e27dd3ead839f1d7739424922c1412c71b50b121
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 13:09:45 -0300

    chore(docs): added jsdocs to useUpdateEffect

--- src/hooks/useUpdateEffect/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect/useUpdateEffect.ts
@@ -1,16 +1,46 @@
-import { DependencyList, EffectCallback, useEffect } from 'react'
+import { DependencyList, EffectCallback, useEffect } from 'react';
 
-import { useIsFirstRender } from '../useIsFirstRender'
+import { useIsFirstRender } from '../useIsFirstRender';
 
+/**
+ * Just modified version of `useEffect` that is skipping the first render.
+ *
+ * See also:
+ *
+ * - `useEffectOnce()`: Inverse of `useUpdateEffect()`
+ * - `useIsFirstRender()`: Return a `boolean`
+ * - `useIsMounted()`: Callback function to avoid Promise execution after component un-mount
+ *
+ * @see https://usehooks-ts.com/react-hook/use-update-effect
+ *
+ * @example
+ * export default function Component() {
+ *   const [data, setData] = useState<number>(0)
+ *   useEffect(() => {
+ *     console.log('Normal useEffect', { data })
+ *   }, [data])
+ * 
+ *   useUpdateEffect(() => {
+ *     console.log('Update useEffect only', { data })
+ *   }, [data])
+ * 
+ *   return (
+ *     <div>
+ *       <p>Open your console</p>
+ *       <button onClick={() => setData(Date.now())}>Update data</button>
+ *     </div>
+ *   )
+ * }
+ */
 function useUpdateEffect(effect: EffectCallback, deps?: DependencyList) {
-  const isFirst = useIsFirstRender()
+  const isFirst = useIsFirstRender();
 
   useEffect(() => {
     if (!isFirst) {
-      return effect()
+      return effect();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps)
+  }, deps);
 }
 
-export default useUpdateEffect
+export default useUpdateEffect;
