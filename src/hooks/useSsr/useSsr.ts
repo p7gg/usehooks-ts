53b96714fbffc693d19b8cd2d77afa248e779019
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:59:14 -0300

    chore(docs): added jsdocs to useSsr

--- src/hooks/useSsr/useSsr.ts
+++ src/hooks/useSsr/useSsr.ts
@@ -1,13 +1,32 @@
+/**
+ * Quickly know where your code will be executed;
+ *
+ * - In the server (Server-Side-Rendering) or
+ * - In the client, the navigator
+ *
+ * This hook doesn't cause an extra render, it just returns the value directly, at the mount time,
+ * and it didn't re-trigger if the value changes.
+ * Otherwise, If you want to be notified when the value changes to react to it, you can use `useIsClient()` instead.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-ssr
+ *
+ * @example
+ * export default function Component() {
+ *   const { isBrowser } = useSsr()
+ *
+ *   return <p>{isBrowser ? 'Browser' : 'Server'}!</p>
+ * }
+ */
 function useSsr() {
   const isDOM =
     typeof window !== 'undefined' &&
     window.document &&
-    window.document.documentElement
+    window.document.documentElement;
 
   return {
     isBrowser: isDOM,
     isServer: !isDOM,
-  }
+  };
 }
 
-export default useSsr
+export default useSsr;
