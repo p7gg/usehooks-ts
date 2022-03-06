044bc7f028767e6b06f43b5189f2bb634cd372da
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 13:07:40 -0300

    chore(docs): added jsdocs to useTimeout

--- src/hooks/useTimeout/useTimeout.ts
+++ src/hooks/useTimeout/useTimeout.ts
@@ -1,28 +1,53 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
-// See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect'
+import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
 
+/**
+ * Very similar to the `useInterval` hook, this React hook implements the native `setTimeout` function keeping the same interface.
+ * You can enable the timeout by setting `delay` as a `number` or disabling it using null.
+ * When the time finishes, the callback function is called.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-timeout
+ * 
+ * @example
+ * export default function Component() {
+ *   const [visible, setVisible] = useState(true)
+ * 
+ *   const hide = () => setVisible(false)
+ * 
+ *   useTimeout(hide, 5000)
+ * 
+ *   return (
+ *     <div>
+ *       <p>
+ *         {visible
+ *           ? "I'm visible for 5000ms"
+ *           : 'You can no longer see this content'}
+ *       </p>
+ *     </div>
+ *   )
+ * }
+ */
 function useTimeout(callback: () => void, delay: number | null) {
-  const savedCallback = useRef(callback)
+  const savedCallback = useRef(callback);
 
   // Remember the latest callback if it changes.
   useIsomorphicLayoutEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
+    savedCallback.current = callback;
+  }, [callback]);
 
   // Set up the timeout.
   useEffect(() => {
     // Don't schedule if no delay is specified.
     // Note: 0 is a valid value for delay.
     if (!delay && delay !== 0) {
-      return
+      return;
     }
 
-    const id = setTimeout(() => savedCallback.current(), delay)
+    const id = setTimeout(() => savedCallback.current(), delay);
 
-    return () => clearTimeout(id)
-  }, [delay])
+    return () => clearTimeout(id);
+  }, [delay]);
 }
 
-export default useTimeout
+export default useTimeout;
