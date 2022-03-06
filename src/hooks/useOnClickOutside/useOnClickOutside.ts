6ac40508feb7c2235f0b609f99045fe9850853d1
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 12:49:21 -0300

    chore(docs): added jsdocs to useOnClickOutside

--- src/hooks/useOnClickOutside/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside/useOnClickOutside.ts
@@ -1,24 +1,55 @@
-import { RefObject } from 'react'
+import { RefObject } from 'react';
 
-import { useEventListener } from '../useEventListener'
+import { useEventListener } from '../useEventListener';
 
-type Handler = (event: MouseEvent) => void
+type Handler = (event: MouseEvent) => void;
 
+/**
+ * React hook for listening for clicks outside of a specified element (see `useRef`).
+ * This can be useful for closing a modal, a dropdown menu etc.
+ *
+ * @see https://usehooks-ts.com/react-hook/use-on-click-outside
+ *
+ * @example
+ * export default function Component() {
+ *   const ref = useRef(null)
+ *
+ *   const handleClickOutside = () => {
+ *     // Your custom logic here
+ *     console.log('clicked outside')
+ *   }
+ *
+ *   const handleClickInside = () => {
+ *     // Your custom logic here
+ *     console.log('clicked inside')
+ *   }
+ *
+ *   useOnClickOutside(ref, handleClickOutside)
+ *
+ *   return (
+ *     <button
+ *       ref={ref}
+ *       onClick={handleClickInside}
+ *       style={{ width: 200, height: 200, background: 'cyan' }}
+ *     />
+ *   )
+ * }
+ */
 function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   ref: RefObject<T>,
   handler: Handler,
   mouseEvent: 'mousedown' | 'mouseup' = 'mousedown',
 ): void {
-  useEventListener(mouseEvent, event => {
-    const el = ref?.current
+  useEventListener(mouseEvent, (event) => {
+    const el = ref?.current;
 
     // Do nothing if clicking ref's element or descendent elements
     if (!el || el.contains(event.target as Node)) {
-      return
+      return;
     }
 
-    handler(event)
-  })
+    handler(event);
+  });
 }
 
-export default useOnClickOutside
+export default useOnClickOutside;
