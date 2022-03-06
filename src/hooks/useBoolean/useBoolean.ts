cd2c13804c41fb5226c2078da7c842eee0565a07
Prodigy7kX <gbos360@hotmail.com>
2022-03-06 01:53:30 -0300

    chore(docs): added jsdocs to useBoolean

--- src/hooks/useBoolean/useBoolean.ts
+++ src/hooks/useBoolean/useBoolean.ts
@@ -8,6 +8,31 @@ interface ReturnType {
   toggle: () => void;
 }
 
+/**
+ * A simple abstraction to play with a boolean, don't repeat yourself.
+ * 
+ * @see https://usehooks-ts.com/react-hook/use-boolean
+ * 
+ * @example
+ * export default function Component() {
+ *   const { value, setValue, setTrue, setFalse, toggle } = useBoolean(false)
+ * 
+ *   // Just an example to use "setValue"
+ *   const customToggle = () => setValue(x => !x)
+ * 
+ *   return (
+ *     <>
+ *       <p>
+ *         Value is <code>{value.toString()}</code>
+ *       </p>
+ *       <button onClick={setTrue}>set true</button>
+ *       <button onClick={setFalse}>set false</button>
+ *       <button onClick={toggle}>toggle</button>
+ *       <button onClick={customToggle}>custom toggle</button>
+ *     </>
+ *   )
+ * }
+ */
 function useBoolean(defaultValue?: boolean): ReturnType {
   const [value, setValue] = useState(!!defaultValue);
 
